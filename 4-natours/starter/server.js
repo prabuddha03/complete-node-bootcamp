650da54ed6a89be73f75b2f8a08319cdb8a097d0
Prabuddha Chowdhury <prabuddha.chowdhury@gmail.com>
2023-06-20 05:45:45 +0530

    1

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -18,7 +18,7 @@ mongoose
     useFindAndModify: false,
   })
   .then((con) => {
-    console.log(con.connections);
+    //console.log(con.connections);
     console.log('DB Connection Successful');
   });
 
