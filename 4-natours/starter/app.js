8034d334b983206a04ffdeb25684c8841f91ddfa
Prabuddha Chowdhury <prabuddha.chowdhury@gmail.com>
2023-07-07 20:13:30 +0530

    gg

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -9,7 +9,7 @@ app.use(express.json()); // middleware
 //Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
+//For unhandled routes 404
 app.all('*', (req, res, next) => {
   res.status(404).json({
     status: 'Failed',
